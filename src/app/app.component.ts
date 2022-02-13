8c18a0f3255591e387e2be275be97092b6c4d1be
lucaliberti <liberti.luca@gmail.com>
2022-02-13 22:06:52 +0100

    finito

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
-  title = 'tile';
+
+  testo_salvato:string="(ancora nulla)"
+
+  constructor() {}
+
+  onClick1(htmlInput:HTMLInputElement) {
+    console.log(htmlInput)
+    this.testo_salvato=htmlInput.value 
+  }
+
+  onClick2( val:string) {
+    console.log(val)
+    this.testo_salvato=val 
+  }
+
+
+
 }
+
+
